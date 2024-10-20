903bdcb4c3f036a37c4773b4b7cfb5e60639acfa
Raymaekers Luca <luca@keyfried.com>
2024-10-21 00:12:02 +0200

    Added common code for messages
    - add: send_message, receive_message functions
    - change: use u8, u16, u32, where possible
    - fix: use PORT in server.c

--- common.c
+++ common.c
@@ -3,8 +3,10 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+
 
 void writef(char *format, ...)
 {
@@ -73,4 +75,61 @@ u8 load_message(struct message *msg, FILE *f)
     msg->text = txt;
 
     return 0;
+}
+
+u32 send_message(struct message msg, u32 serverfd)
+{
+    // stream length : message author : message timestamp : message text
+    u32 buf_len = sizeof(buf_len) + MESSAGE_AUTHOR_LEN + MESSAGE_TIMESTAMP_LEN + msg.len;
+    char buf[buf_len];
+    u32 offset;
+
+    memcpy(buf, &buf_len, sizeof(buf_len));
+    offset = sizeof(buf_len);
+    memcpy(buf + offset, msg.author, MESSAGE_AUTHOR_LEN);
+    offset += MESSAGE_AUTHOR_LEN;
+    memcpy(buf + offset, msg.timestamp, MESSAGE_TIMESTAMP_LEN);
+    offset += MESSAGE_TIMESTAMP_LEN;
+    memcpy(buf + offset, msg.text, msg.len);
+
+    u32 n = send(serverfd, &buf, buf_len, 0);
+    if (n == -1)
+        return n;
+
+    writef("%d bytes sent.\n", n);
+    return n;
+}
+
+u32 receive_message(struct message *msg, u32 clientfd)
+{
+    // must all be of the s
+    u32 nrecv, buf_len;
+    // limit on what can be received with recv()
+    u32 buf_size = 20;
+    // temporary buffer to receive message data over a stream
+    char recv_buf[BUF_MAX];
+
+    nrecv = recv(clientfd, recv_buf, buf_size, 0);
+    if (nrecv == 0 || nrecv == -1)
+        return nrecv;
+
+    memcpy(&buf_len, recv_buf, sizeof(buf_len));
+
+    u32 i = 0;
+    while (nrecv < buf_len) {
+        // advance the copying by the amounts of bytes received each time
+        i = recv(clientfd, recv_buf + nrecv, buf_size, 0);
+        if (i == 0 || i == -1)
+            return nrecv;
+        nrecv += i;
+    }
+
+    struct message received = {0};
+    memcpy(&received, recv_buf + sizeof(buf_len), MESSAGE_AUTHOR_LEN + MESSAGE_TIMESTAMP_LEN);
+    received.text = recv_buf + sizeof(buf_len) + MESSAGE_AUTHOR_LEN + MESSAGE_TIMESTAMP_LEN;
+    received.len  = buf_len - sizeof(buf_len) - MESSAGE_AUTHOR_LEN - MESSAGE_TIMESTAMP_LEN;
+
+    // assume clientfd is serverfd + 1;
+    writef("Received %d bytes from client(%d): %s [%s] %s\n", nrecv, clientfd - 3, received.timestamp, received.author, received.text);
+    return nrecv;
 }
--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -14,7 +15,7 @@
 static const char *filename = "history.dat";
 
 enum { FD_SERVER = 0 };
-int serverfd;
+u32 serverfd;
 
 void err_exit(const char *msg)
 {
@@ -27,9 +28,9 @@ void err_exit(const char *msg)
 
 int main(void)
 {
-    int clientfd;
-    int nclient             = 0;
-    int on                  = 1;
+    u32 clientfd;
+    u16 nclient             = 0;
+    u8 on                  = 1;
     struct message msg_recv = {0};
 
     serverfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
@@ -38,7 +39,7 @@ int main(void)
 
     const struct sockaddr_in address = {
         AF_INET,
-        htons(9999),
+        htons(PORT),
         {0},
     };
 
@@ -60,7 +61,7 @@ int main(void)
     };
 
     for (;;) {
-        int ret = poll(fds, FD_MAX, 50000);
+        u32 ret = poll(fds, FD_MAX, 50000);
         if (ret == -1)
             err_exit("Error while polling");
         else if (ret == 0) {
@@ -85,7 +86,7 @@ int main(void)
                 nclient++;
 
                 // get a new available spot in the fds array
-                int i;
+                u32 i;
                 for (i = 0; i < MAX_CONNECTIONS; i++)
                     if (fds[i].fd == -1)
                         break;
@@ -98,15 +99,15 @@ int main(void)
         }
 
         // Check for events on connected clients
-        for (int i = 1; i <= nclient; i++) {
+        for (u32 i = 1; i <= nclient; i++) {
             if (!(fds[i].revents & POLLIN))
                 continue;
 
-            int nrecv;
+            u32 nrecv;
 
             clientfd = fds[i].fd;
-            nrecv    = recv(clientfd, &msg_recv, sizeof(struct message), 0);
 
+            nrecv = receive_message(&msg_recv, clientfd);
             if (nrecv == 0) {
                 printf("client %d disconnected.\n", i);
                 fds[i].fd      = -1;
@@ -122,10 +123,8 @@ int main(void)
                 err_exit("Error while receiving from client socket.");
             }
 
-            printf("client %d sent %d bytes.\n", i, nrecv);
-
             // TODO:
-            for (int j = 1; j <= nclient; j++) {
+            for (u32 j = 1; j <= nclient; j++) {
                 // skip the client that sent the message
                 if (j == i)
                     continue;
@@ -136,10 +135,11 @@ int main(void)
             }
 
             // // TODO: Serialize received message
-            FILE *f = fopen(filename, "wb");
-            save_message(&msg_recv, f);
-            fclose(f);
-            // return 0;
+            // FILE *f = fopen(filename, "wb");
+            // save_message(&msg_recv, f);
+            // fclose(f);
+            // // return 0;
+
         }
     }
 
