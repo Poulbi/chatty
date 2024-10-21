abc4dc5fb0b440172f1594d9488f3114dcc9b19b
Raymaekers Luca <luca@keyfried.com>
2024-10-21 12:52:46 +0200

    Add compile_flags

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-gcc -g -Wall -pedantic -std=c99 -o chatty client.c common.c
-gcc -g -Wall -pedantic -std=c99 -o server server.c common.c
-gcc -g -Wall -pedantic -std=c99 -o send send.c common.c
-gcc -g -Wall -pedantic -std=c99 -o recv recv.c common.c
+gcc -g -Wall -pedantic -std=c99 -o chatty client.c
+gcc -g -Wall -pedantic -std=c99 -o server server.c
+gcc -g -Wall -pedantic -std=c99 -o send send.c
+gcc -g -Wall -pedantic -std=c99 -o recv recv.c
